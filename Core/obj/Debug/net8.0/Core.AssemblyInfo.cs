2e2e021126e6c81b396d6eb86d00a2451f71feb5
AzizeDirilik <azizedirilik6@gmail.com>
2025-01-03 15:22:19 +0300

    JWT

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d8f2104dedc6e55f5203cc65b4323c32a439e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40101bc063a4019a0bfe3365d22c237e964d4cf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
